52f2f44d72be360208de0433aecb4360a36301b1
luism <luis.marting@alumnos.upm.es>
2024-05-02 11:48:24 +0200

    Tiempo añadido

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,7 +1,3 @@
 extends Node
 
-@export var barraVida_path: NodePath 
-
-func change_heart_bar(new_value) -> void:
-	var barraVida: Range = get_node(barraVida_path)
-	barraVida.value = new_value / 0.04
+@export var segundos: int
--- Scripts/jugadorUI.gd
+++ Scripts/jugadorUI.gd
@@ -1,10 +1,16 @@
 extends CanvasLayer
 
-
 func _ready():
 	if get_parent().has_node("Player"):
 		$barraVida.value = get_parent().get_node("Player").salud
+	$HBoxContainer/Label.text = str(get_parent().segundos)
 
 
 func _process(delta):
 	$barraVida.value = get_parent().get_node("Player").salud
+
+
+func _on_timer_timeout():
+	if get_parent().segundos > 0:
+		get_parent().segundos -= 1
+		$HBoxContainer/Label.text = str(get_parent().segundos)
