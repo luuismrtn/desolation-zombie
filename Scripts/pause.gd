ddf7260d1e0bc896a5d019ea2798e4303e8427d3
luism <unconfigured@null.spigotmc.org>
2024-05-20 14:13:15 +0200

    Pausa arreglada

--- Scripts/pause.gd
+++ Scripts/pause.gd
@@ -1,10 +1,9 @@
-extends Control
-
-@onready var main = $"../../"
+extends CanvasLayer
 
 func _on_resume_pressed():
-	main.pauseMenu()
+	Global.pauseMenu()
 
 
 func _on_quit_pressed():
-	get_tree().quit()
+	Global.pauseMenu()
+	get_tree().change_scene_to_file("res://Escenas/menu.tscn")
