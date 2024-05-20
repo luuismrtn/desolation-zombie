6a4b6ce2d82496776924e2d18981fbe3942ee784
diegoManz <118812661+diegoManz@users.noreply.github.com>
2024-05-20 12:07:04 +0200

    zombis randomizados

--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -9,9 +9,12 @@ var objetivo
 
 var fuerte = false
 
+var damage = 5
+
 func _ready():
 	if(randf_range(0,1) > 0.7):
 		fuerte = true
+		damage = 10
 		$ZombieSkin.texture = load("res://Sprites/Zombie/Zombi fuerte.png")
 		$ZombieSkin/AnimatedSprite2D.sprite_frames = load("res://Sprites/Zombie/zombieFuerteWalk.tres")
 	var mapa = Global.mapa
@@ -32,4 +35,4 @@ func _physics_process(delta):
 
 	var angle_to_player = direction.angle()
 	rotation = angle_to_player
-	
+
